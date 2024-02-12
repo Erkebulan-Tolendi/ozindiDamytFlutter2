f00972f70502250abc36ecc3bc039d1086f4f09e
Erkebulan <tolendierkebulan555@gmail.com>
2024-02-12 11:19:30 +0600

    home pag eis added

--- lib/features/bottom_nav_bar/home_page/home_page.dart
+++ lib/features/bottom_nav_bar/home_page/home_page.dart
@@ -1,6 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:ozindi_damyt/core/navigation/navigation.dart';
 
+List<Map<String, dynamic>> user_info = [
+  {
+    "name": "Кітап оқу",
+    "type_info": "бет",
+    "info": "843",
+    "photo": "assets/icons/book_icon.png",
+  },
+  {
+    "name": "Quiz",
+    "type_info": "балл",
+    "info": "280",
+    "photo": "assets/icons/question-mark.png",
+  },
+  {
+    "name": "Спорт",
+    "type_info": "мин",
+    "info": "152",
+    "photo": "assets/icons/biceps.png",
+  },
+];
+
 class HomePage extends StatelessWidget {
   const HomePage({Key? key});
 
@@ -56,13 +77,111 @@ class HomePage extends StatelessWidget {
                 ],
               ),
             ),
-            centerTitle: true, // Center the title in the appBar
+            centerTitle: true,
           ),
         ),
       ),
-      body: Center(
-        child: Text("Home Page"),
-      ),
+      body: ListAction(),
+    );
+  }
+}
+
+class ListAction extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        SizedBox(
+          height: 280,
+          child: ListInfo(),
+        )
+      ],
+    );
+  }
+}
+
+class ListInfo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      scrollDirection: Axis.vertical,
+      itemCount: user_info.length,
+      itemBuilder: (context, index) {
+        return InkWell(
+          onTap: () {},
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.5),
+                  spreadRadius: 5,
+                  blurRadius: 7,
+                  offset: Offset(0, 3),
+                ),
+              ],
+            ),
+            margin: EdgeInsets.all(8),
+            height: 70,
+            child: CardKino(
+              index: index,
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
+
+class CardKino extends StatelessWidget {
+  int index;
+  CardKino({required this.index});
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        SizedBox(
+          width: 20,
+        ),
+        Image.asset(
+          user_info[index]["photo"],
+          width: 35,
+          height: 35,
+        ),
+        SizedBox(
+          width: 10,
+        ),
+        Padding(
+          padding: const EdgeInsets.only(
+            left: 8.0,
+          ),
+          child: SizedBox(
+              width: 120,
+              child: Text(
+                user_info[index]["name"],
+                style: TextStyle(fontSize: 23, fontWeight: FontWeight.bold),
+              )),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 100.0),
+          child: Row(
+            children: [
+              Text(
+                user_info[index]["info"],
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                width: 5,
+              ),
+              Text(
+                user_info[index]["type_info"],
+                style: TextStyle(fontSize: 16),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
